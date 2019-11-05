03e4f2003bf15e42cb4b8d841748cf1cc807b66e
fengchuiguo1994 <huang182@live.cn>
2019-11-05 21:09:14 +0800

    20191105

--- C++/1.35multiples.cpp
+++ C++/1.35multiples.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 int main(){
-    cout << "hello world" << endl;
+    clock_t start = clock();
+    int num = 1000;
+    int total = 0;
+    for(int i=1;i<num;i++){
+        if(i%3 == 0 || i%5==0){
+            total += i;
+        }
+    }
+    cout << total << endl;
+    clock_t finish = clock();
+    double consumeTime = (double)(finish-start)/CLOCKS_PER_SEC;
+    cout << "use time" << consumeTime << "s" << endl;
     return 0;
 }
